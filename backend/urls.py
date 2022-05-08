1c71eb49002c28f8a49aa417c7bdbb55653db357
Emilie Allen <emilie.allen@hotmail.co.uk>
2022-05-08 16:23:16 +0100

    finalise deploymnet

--- backend/urls.py
+++ backend/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pictures/', include('pictures.urls')),
-    path('auth/', include('users.urls'))
+    path('api/pictures/', include('pictures.urls')),
+    path('api/auth/', include('users.urls'))
 ]
